d3ad2710be1a12b49c53796cb4d94d008370493b
Pim Hakkert <43008188+pimhakkert@users.noreply.github.com>
2020-04-28 14:45:39 +0200

    Disable zoom

--- resources/views/additions/header.blade.php
+++ resources/views/additions/header.blade.php
@@ -2,7 +2,10 @@
 <html lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta
+        name='viewport'
+        content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
+    />
     <title>@yield('title')</title>
     <link rel="stylesheet" href="{{ asset('css/additions/header.css') }}">
     <link rel="stylesheet" href="{{ asset('css/additions/footer.css') }}">
