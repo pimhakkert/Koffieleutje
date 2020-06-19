abb8a859284ed0d654678b0b18ebf23ce31d7b79
XRaiderGH <raigriffioen3@gmail.com>
2020-06-19 15:32:29 +0200

    made start at login

--- resources/views/login/main.blade.php
+++ resources/views/login/main.blade.php
@@ -4,23 +4,31 @@
 @section('css', asset('css/login/main.css'))
 
 @section('content')
-    <div class="section app-background">
-        <div class="wrap">
-            <div class="loginContainer">
-                <div class="top-row">
-                    <h1>Don't have an account?</h1>
+    <div class="login-section">
+
+          <div class="row coffee-box-title">
+              <h1 class="col-7" data-aos="fade-right"  data-aos-duration="1000">Customize your box</h1>
+            </div>
+
+            <div class="loginContainer app-background">
+
+                <div class="row">
+                   <div class="col-12 top-row">
+                       <h2 class="item">Don't have an account?</h2>
+                       <a href="#" class="btn-register item">Register</a>
+                   </div>
                 </div>
 
-                <div class="bottom-row">
-                    <div class="col-7">
-                        test
+                <div class="bottom-row row">
+                    <div class="left col-7">
+                        <img src="imgs/logo_white.svg" alt="">
                     </div>
-                    <div class="col-5">
+                    <div class="right col-5">
                         test
                     </div>
                 </div>
+
             </div>
-        </div>
     </div>
 @endsection
 
